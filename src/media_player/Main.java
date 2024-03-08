0c84776c119062dccf501db0737a17b85cc33a51
Ciro Vitiello <ciro.riccione@gmail.com>
2024-03-08 12:04:34 +0100

    audio finished, missing methods for video and image

---
+++ src/media_player/Main.java
@@ -0,0 +1,37 @@
+package media_player;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(" Audio player settings ");
+        Audio firstSong = new Audio("FirstTry", 3);
+        firstSong.play();
+        firstSong.turnDownVolume();
+        System.out.println(firstSong);
+        firstSong.play();
+        firstSong.turnDownVolume();
+        firstSong.play();
+        firstSong.turnDownVolume();
+        firstSong.play();
+        System.out.println(firstSong);
+        firstSong.turnDownVolume();
+        firstSong.turnDownVolume();
+        firstSong.turnDownVolume();
+        System.out.println(firstSong);
+        firstSong.play();
+        firstSong.turnUpVolume();
+        firstSong.turnUpVolume();
+        firstSong.play();
+        firstSong.turnUpVolume();
+        firstSong.turnUpVolume();
+        firstSong.turnUpVolume();
+        firstSong.turnUpVolume();
+        firstSong.turnUpVolume();
+        firstSong.play();
+        firstSong.turnUpVolume();
+        firstSong.turnUpVolume();
+        firstSong.turnUpVolume();
+        System.out.println(firstSong);
+        firstSong.play();
+    }
+}
